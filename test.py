4f2395cecb4b3f0f7dcb522b66647de55515c03d
Phantomaxxx <42619740+Phantomaxxx@users.noreply.github.com>
2019-06-14 21:59:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import tarfile
 
 repo = Repo(".")
 file="tmp.tar"
-with open(file, "w") as tar: repo.archive(tar, prefix="o1/", path=".generated/") 
+with open(file, "w") as tar: repo.archive(tar, prefix=".tags/c001/", path=".generated/") 
 with tarfile.open(file, mode="r|") as tar: tar.extractall()
 os.remove(file)
 
